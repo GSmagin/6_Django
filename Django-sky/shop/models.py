88f151cc70d2dec15c3eeb2fcd7ef2e295dbdca4
GSmagin <Grog512new@mail.ru>
2024-06-30 02:20:25 +0300

    Правки адреса

--- Django-sky/shop/models.py
+++ Django-sky/shop/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     weight = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Вес товара', null=True, blank=True)
-    image = models.ImageField(upload_to='products/', verbose_name='Изображение (превью)', null=True, blank=True)
+    image = models.ImageField(upload_to='shop/', verbose_name='Изображение (превью)', null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
